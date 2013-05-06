947acaacef0143dddbdae2ba5999b53c263ed132
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-05-06 19:55:51 +0300

    Release 0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ll.policy',
-    version='0.4',
+    version='0.5',
     description="Turns Plone Site into LL site.",
     long_description=open("README.rst").read(),
     classifiers=[
